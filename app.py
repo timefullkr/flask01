3502607f2dfc593c2c2a0564d99a73c952d694fa
aaa <timefullkr@naver.com>
2023-09-01 08:10:34 +0900

    update

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/hello")
 def hello_world():
     return "Hello, World!"
+
+@app.route("/")
+def index():
+    return send_from_directory("html","index.html")
+
+@app.route("/<path:name>")
+def page(name):
+    return send_from_directory("html",name)
 
 if __name__ == "__main__":
     app.run(debug=True)
